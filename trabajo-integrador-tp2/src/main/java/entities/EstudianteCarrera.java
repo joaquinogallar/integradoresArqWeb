b8ef873999bea153de41c6592518b3f3c269a6b9
Joaquin <joaquinnogallar@gmail.com>
2024-09-26 09:16:16 -0300

    fix: conflicto EstudianteCarrera

--- trabajo-integrador-tp2/src/main/java/entities/EstudianteCarrera.java
+++ trabajo-integrador-tp2/src/main/java/entities/EstudianteCarrera.java
@@ -6,11 +6,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Entity
-<<<<<<< HEAD:trabajo-integrador-tp2/src/main/java/entities/EstudianteCarrera.java
 @Table(name = "estudiante_carrera")
-=======
-@Table(name = "carrera_inscripta")
->>>>>>> ffec451d33b52ddc79a39bd85daed9f178e7aa3e:trabajo-integrador-tp2/src/main/java/entities/CarreraInscripta.java
 @Data
 @NoArgsConstructor
 public class EstudianteCarrera {
@@ -19,7 +15,6 @@ public class EstudianteCarrera {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-<<<<<<< HEAD:trabajo-integrador-tp2/src/main/java/entities/EstudianteCarrera.java
     @ManyToOne
     private Estudiante estudiante;
     @ManyToOne
@@ -30,20 +25,3 @@ public class EstudianteCarrera {
 
 
 }
-=======
-    private String nombreCarrera;
-    private int antiguedad;
-    private boolean graduado;
-
-    @ManyToOne
-    @JoinColumn(name = "estudiante_id")
-    private Estudiante estudiante;
-
-    public CarreraInscripta(String nombreCarrera, Estudiante estudiante) {
-        this.nombreCarrera = nombreCarrera;
-        this.estudiante = estudiante;
-        antiguedad = 0;
-        graduado = false;
-    }
-}
->>>>>>> ffec451d33b52ddc79a39bd85daed9f178e7aa3e:trabajo-integrador-tp2/src/main/java/entities/CarreraInscripta.java
