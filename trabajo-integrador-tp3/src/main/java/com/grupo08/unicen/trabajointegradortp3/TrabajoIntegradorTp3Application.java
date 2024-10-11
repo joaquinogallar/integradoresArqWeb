dd559fc363020245de723cd269d657292f1356e0
Joaquin Ogallar <joaquinnogallar@gmail.com>
2024-10-11 10:29:46 -0300

    feat: estructura del trabajo n3

---
+++ trabajo-integrador-tp3/src/main/java/com/grupo08/unicen/trabajointegradortp3/TrabajoIntegradorTp3Application.java
@@ -0,0 +1,13 @@
+package com.grupo08.unicen.trabajointegradortp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoIntegradorTp3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrabajoIntegradorTp3Application.class, args);
+    }
+
+}
---
+++ trabajo-integrador-tp3/src/test/java/com/grupo08/unicen/trabajointegradortp3/TrabajoIntegradorTp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo08.unicen.trabajointegradortp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoIntegradorTp3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
