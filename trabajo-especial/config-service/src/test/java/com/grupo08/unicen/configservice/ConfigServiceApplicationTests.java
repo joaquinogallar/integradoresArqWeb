708afac9eed7e6419b3a8ff2e3299b1abd6bffe6
Joaco <joaquinnogallar@gmail.com>
2024-11-05 10:34:19 -0300

    feat: inicializacion del proyecto con microservicios, crea el servicio de configuracion (config-service)

---
+++ trabajo-especial/config-service/src/test/java/com/grupo08/unicen/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo08.unicen.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
