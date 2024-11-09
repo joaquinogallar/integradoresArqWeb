75c700f1fc8f0bfd34ddf24bbfb2179427529396
Joaco <joaquinnogallar@gmail.com>
2024-11-09 15:21:21 -0300

    feat: microservicio de viaje

---
+++ trabajo-especial/microservice-journey/src/main/java/com/grupo08/unicen/microservicejourney/MicroserviceJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.grupo08.unicen.microservicejourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceJourneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceJourneyApplication.class, args);
+    }
+
+}
---
+++ trabajo-especial/microservice-journey/src/test/java/com/grupo08/unicen/microservicejourney/MicroserviceJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo08.unicen.microservicejourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceJourneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
