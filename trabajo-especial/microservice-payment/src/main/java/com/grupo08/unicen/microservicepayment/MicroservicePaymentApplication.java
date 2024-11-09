f481a2ae07984da8b3f2706e9fd09e920a796c96
Joaco <joaquinnogallar@gmail.com>
2024-11-09 17:00:11 -0300

    feat: microservicio de pagos y tarifas

---
+++ trabajo-especial/microservice-payment/src/main/java/com/grupo08/unicen/microservicepayment/MicroservicePaymentApplication.java
@@ -0,0 +1,13 @@
+package com.grupo08.unicen.microservicepayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicePaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicePaymentApplication.class, args);
+    }
+
+}
---
+++ trabajo-especial/microservice-payment/src/test/java/com/grupo08/unicen/microservicepayment/MicroservicePaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo08.unicen.microservicepayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicePaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
