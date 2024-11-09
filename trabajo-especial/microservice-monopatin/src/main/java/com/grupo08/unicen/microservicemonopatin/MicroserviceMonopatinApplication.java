6e6db5508838d9a4fd0418bb9f5b01205377d1c3
Joaco <joaquinnogallar@gmail.com>
2024-11-09 11:46:51 -0300

    feat: microservicio de monopatines y paradas, configura las bases de datos para los microservicios monopatin y user

---
+++ trabajo-especial/microservice-monopatin/src/main/java/com/grupo08/unicen/microservicemonopatin/MicroserviceMonopatinApplication.java
@@ -0,0 +1,13 @@
+package com.grupo08.unicen.microservicemonopatin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceMonopatinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceMonopatinApplication.class, args);
+    }
+
+}
---
+++ trabajo-especial/microservice-monopatin/src/test/java/com/grupo08/unicen/microservicemonopatin/MicroserviceMonopatinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo08.unicen.microservicemonopatin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceMonopatinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
