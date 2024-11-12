b6a2ae335ef1b80c49d8e83405f69cad378a1e57
Joaco <joaquinnogallar@gmail.com>
2024-11-11 22:03:20 -0300

    update: journeyFeignClient

--- trabajo-especial/microservice-user/src/main/java/com/grupo08/unicen/microserviceuser/client/JourneyFeignClient.java
+++ trabajo-especial/microservice-user/src/main/java/com/grupo08/unicen/microserviceuser/client/JourneyFeignClient.java
@@ -2,12 +2,18 @@ package com.grupo08.unicen.microserviceuser.client;
 
 import com.grupo08.unicen.microserviceuser.model.JourneyDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 @FeignClient(name = "microservice-journey")
 public interface JourneyFeignClient {
     @GetMapping("/api/journeys/")
-    List<JourneyDto> getAllJourneys();
+    public ResponseEntity<List<JourneyDto>> getAllJourneys();
+
+    @PostMapping("/api/journeys")
+    public ResponseEntity<String> createJourney();
+
 }
