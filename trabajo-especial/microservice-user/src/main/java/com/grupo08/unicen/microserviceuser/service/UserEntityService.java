1a1b90a2b333ae32d363b716de0d10ef7b15e693
Joaquin Ogallar <joaquinnogallar@gmail.com>
2024-11-12 15:05:03 -0300

    feat: agrega verificacion de que el usuario tiene la cuenta que quiere usar para el viaje

--- trabajo-especial/microservice-user/src/main/java/com/grupo08/unicen/microserviceuser/service/UserEntityService.java
+++ trabajo-especial/microservice-user/src/main/java/com/grupo08/unicen/microserviceuser/service/UserEntityService.java
@@ -5,8 +5,10 @@ import com.grupo08.unicen.microservicemonopatin.entity.Monopatin;
 import com.grupo08.unicen.microserviceuser.client.JourneyFeignClient;
 import com.grupo08.unicen.microserviceuser.client.MonopatinFeignClient;
 import com.grupo08.unicen.microserviceuser.dto.UserEntityDto;
+import com.grupo08.unicen.microserviceuser.entity.Account;
 import com.grupo08.unicen.microserviceuser.entity.UserEntity;
 import com.grupo08.unicen.microserviceuser.exception.UserNotFoundException;
+import com.grupo08.unicen.microserviceuser.repository.AccountRepository;
 import com.grupo08.unicen.microserviceuser.repository.UserEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,13 +23,15 @@ import java.util.UUID;
 public class UserEntityService {
 
     private final UserEntityRepository userEntityRepository;
+    private final AccountRepository accountRepository;
     private MonopatinFeignClient monopatinFeignClient;
     @Autowired
     private JourneyFeignClient journeyFeignClient;
 
     // dependency injection
-    public UserEntityService(UserEntityRepository userEntityRepository, MonopatinFeignClient monopatinFeignClient) {
+    public UserEntityService(UserEntityRepository userEntityRepository, AccountRepository accountRepository, MonopatinFeignClient monopatinFeignClient) {
         this.userEntityRepository = userEntityRepository;
+        this.accountRepository = accountRepository;
         this.monopatinFeignClient = monopatinFeignClient;
     }
 
@@ -70,8 +74,14 @@ public class UserEntityService {
         return ResponseEntity.ok(userDto);
     }
 
-    public ResponseEntity<JourneyDto> activateMonopatinByQr(UUID monopatinId, UUID userId) {
+    public ResponseEntity<JourneyDto> activateMonopatinByQr(UUID monopatinId, UUID userId, UUID accountId) {
         try {
+            UserEntity user = userEntityRepository.findById(userId).orElseThrow(() -> new UserNotFoundException(userId.toString()));
+            Account account = accountRepository.findById(accountId).orElseThrow(() -> new AccountNotFoundException(accountId.toString()));
+
+            if(!user.getAccounts().contains(account))
+                throw new AccountNotFoundException(accountId.toString());
+
             Monopatin monopatin = monopatinFeignClient.getMonopatinById(monopatinId).getBody();
             return journeyFeignClient.createViaje(monopatinId, userId);
         } catch (Exception e) {
