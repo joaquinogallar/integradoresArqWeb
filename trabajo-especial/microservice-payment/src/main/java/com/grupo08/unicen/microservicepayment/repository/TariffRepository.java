6e3417864a01101323d24e1d1cea4eb460481d4d
Joaco <joaquinnogallar@gmail.com>
2024-11-13 20:44:51 -0300

    delete: microservicio de pagos (microservice-payment)

--- trabajo-especial/microservice-payment/src/main/java/com/grupo08/unicen/microservicepayment/repository/TariffRepository.java
+++
@@ -1,7 +0,0 @@
-package com.grupo08.unicen.microservicepayment.repository;
-
-import com.grupo08.unicen.microservicepayment.entity.Tariff;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface TariffRepository extends JpaRepository<Tariff, UUID> {}
--- trabajo-especial/microservice-payment/src/main/java/com/grupo08/unicen/microservicepayment/repository/TransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.grupo08.unicen.microservicepayment.repository;
-
-import com.grupo08.unicen.microservicepayment.entity.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface TransactionRepository extends JpaRepository<Transaction, UUID> {}
