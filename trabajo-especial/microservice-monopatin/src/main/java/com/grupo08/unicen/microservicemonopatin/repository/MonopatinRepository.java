932557f0a3ea342ccb88cdd3e28f1771e952b2ff
Alejo Rau <105177309+AlejoRau@users.noreply.github.com>
2024-11-14 21:13:18 -0300

    feignClient

--- trabajo-especial/microservice-monopatin/src/main/java/com/grupo08/unicen/microservicemonopatin/repository/MonopatinRepository.java
+++ trabajo-especial/microservice-monopatin/src/main/java/com/grupo08/unicen/microservicemonopatin/repository/MonopatinRepository.java
@@ -20,9 +20,6 @@ public interface MonopatinRepository extends JpaRepository<Monopatin, UUID> {
     @Query("SELECT m from Monopatin m order by m.useTime desc")
     List<Monopatin> getMonopatinesConTiempoPausa();
 
-    @Query("SELECT m FROM Monopatin m")
-    List<Monopatin> getMonopatinesSinTiempoPausa();
-
     @Query("SELECT m from Monopatin m where m.state = :IN_MAINTENANCE ")
     List<Monopatin>getMonopatinesInMaintenance();
 
