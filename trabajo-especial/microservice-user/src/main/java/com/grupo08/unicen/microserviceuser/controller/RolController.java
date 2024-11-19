acdf56a07ee60ccbe8a94b63a4e0393f58a2b8ef
Joaco <joaquinnogallar@gmail.com>
2024-11-19 19:39:21 -0300

    remove: anotacion @EnableDiscoveryClient

--- trabajo-especial/microservice-user/src/main/java/com/grupo08/unicen/microserviceuser/controller/RolController.java
+++ trabajo-especial/microservice-user/src/main/java/com/grupo08/unicen/microserviceuser/controller/RolController.java
@@ -1,19 +1,13 @@
 package com.grupo08.unicen.microserviceuser.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.grupo08.unicen.microserviceuser.dto.RolDto;
 
 @RestController
 @RequestMapping("/api/roles")
 public class RolController {
-/*    @Autowired
+/*
+    @Autowired
     RolService rolService;
 
     @PostMapping("")
@@ -21,6 +15,7 @@ public class RolController {
 
             return rolService.save(rolDto);
 
-    }*/
+    }
+*/
 
 }
